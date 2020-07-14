b5033312fb6ec12d8f62ded936cdb6f277846bf1
Amad Salmon <amad.salmon@etu.univ-cotedazur.fr>
2020-07-14 13:09:11 +0200

    Add struct Story in Model

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,8 +14,20 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    let stories = [
+        Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right."),
+        Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead."),
+        Story(title: "You find a treasure chest.", choice1: "Open it.", choice2: "Check for traps.")
+    
+    ]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let example = stories[0]
+        storyLabel.text = example.title
+        choice1Button.setTitle(example.choice1, for: UIControl.State.normal)
+        choice2Button.setTitle(example.choice2, for: UIControl.State.normal)
 
     }
 
